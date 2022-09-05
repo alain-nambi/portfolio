812d3a40f24ac84b8fc405e30709938efc702d3f
alain <alainnambi@gmail.com>
2022-09-05 14:42:14 +0300

    Add {Contact, Home} component to Main page App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import Contact from "./components/Contact/Contact"
+import Home from "./components/Home/Home"
+
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
+const style = {display: "flex", gap: "1rem", padding: "1rem"}
+
 function App() {
   return (
-    <div className="App">
-      <p>Bienvenu dans React + Vite</p>
-    </div>
+    <>
+      <div className="App">
+        <h1>Portfolio</h1>
+      </div>
+
+      <BrowserRouter basename="">
+        <nav style={style}>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact Page</Link> <br />
+        </nav>
+
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+    
   )
 }
 
