e02c636ba29931e98dc0430f43d03768d3c16476
alain <alainnambi@gmail.com>
2022-09-07 08:15:54 +0300

    Remove style and add build to gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Contact from "./components/Contact/Contact"
-import Home from "./components/Home/Home"
-
 import { HashRouter, Link, Routes, Route } from "react-router-dom";
 
-const style = {display: "flex", gap: "1rem", padding: "1rem"}
+import Contact from "./components/Contact/Contact"
+import Home from "./components/Home/Home"
 
 function App() {
   return (
@@ -13,7 +11,7 @@ function App() {
       </div>
 
       <HashRouter basename="/">
-        <nav style={style}>
+        <nav>
           <Link to="/">Home</Link>
           <Link to="/contact">Contact Page</Link> <br />
         </nav>
