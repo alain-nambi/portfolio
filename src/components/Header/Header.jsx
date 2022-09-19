15cc9b3db6861b0b9b4b26974794bc9646b4369f
alain <alainnambi@gmail.com>
2022-09-19 16:19:15 +0300

    Change logo's name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         <header className="header">
             <nav className="nav container">
                 <a href="#" className="nav__logo">
-                    Alain
+                    Alain Nambii
                 </a>
 
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
