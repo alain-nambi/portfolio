210d04cb12574a6aad5fa422ff644d81e406e013
alain <alainnambi@gmail.com>
2022-09-19 13:46:40 +0300

    Update {App, Header} CSS : ▶️ Add color to App.css ▶️ Update Header.css ▶️ Import Header.css to Header.jsx component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Header.css";
+
 const Header = () => {
     return (
         <header className="header">
@@ -11,42 +13,42 @@ const Header = () => {
                 <div className="nav__menu">
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav_link">
+                            <a href="#home" className="nav__link active-link">
                                 <i className="uil uil-estate nav__icon"></i>
                                 Home
                             </a>
                         </li>
 
                         <li className="nav__item">
-                            <a href="#about" className="nav_link">
+                            <a href="#about" className="nav__link">
                                 <i className="uil uil-user nav__icon"></i>
                                 About
                             </a>
                         </li>
 
                         <li className="nav__item">
-                            <a href="#skills" className="nav_link">
+                            <a href="#skills" className="nav__link">
                                 <i className="uil uil-file nav__icon"></i>
                                 Skills
                             </a>
                         </li>
 
                         <li className="nav__item">
-                            <a href="#services" className="nav_link">
+                            <a href="#services" className="nav__link">
                                 <i className="uil uil-briefcase-alt nav__icon"></i>
                                 Services
                             </a>
                         </li>
 
                         <li className="nav__item">
-                            <a href="#portfolio" className="nav_link">
+                            <a href="#portfolio" className="nav__link">
                                 <i className="uil uil-scenery nav__icon"></i>
                                 Portfolio
                             </a>
                         </li>
 
                         <li className="nav__item">
-                            <a href="#contact" className="nav_link">
+                            <a href="#contact" className="nav__link">
                                 <i className="uil uil-message nav__icon"></i>
                                 Contact
                             </a>
